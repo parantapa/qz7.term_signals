1f1645be16225fe20b682fa6e3acd2ee874b09a7
Parantapa Bhattacharya <pb@parantapa.net>
2019-05-30 15:19:15 -0400

    Add raise_once parameter and better docstrings

--- qz7/term_signals/__init__.py
+++ qz7/term_signals/__init__.py
@@ -1,18 +1,29 @@
 """
-Functions for gracefully handling TERM signals.
+Gracefully handle term signals.
+
+Term signals handled by default are: SIGTERM, SIGINT, SIGHUP
 """
 
 import signal
 import logging
 from functools import partial
 
-TERM_SIGNALS = ["SIGINT", "SIGQUIT", "SIGHUP", "SIGTERM"]
+TERM_SIGNALS = ["SIGINT", "SIGHUP", "SIGTERM"]
 
 TERM_SIGNAL_RECEIVED = []
 
 log = logging.getLogger(__name__)
 
 class TermSignalException(KeyboardInterrupt):
+    """
+    Raised when a term signal is received.
+
+    Attributes:
+        signame: Name of the signal, e.g. SIGINT
+        signum: Number of the signal, e.g. 2 for SIGINT
+        frame: Stack frame
+    """
+
     def __init__(self, signame, signum, frame):
         super().__init__("Received %s(%d)" % (signame, signum))
 
@@ -20,16 +31,19 @@ class TermSignalException(KeyboardInterrupt):
         self.signum = signum
         self.frame = frame
 
-def handle_term_signal(signame, raise_exc, signum, frame):
+def _handle_term_signal(signame, raise_exc, raise_once, signum, frame):
     """
     Handle the given term signal.
     """
 
     log.info("Received %s(%d)", signame, signum)
 
-    TERM_SIGNAL_RECEIVED.append(signame)
+    TERM_SIGNAL_RECEIVED.append((signame, signum))
 
     if raise_exc:
+        if raise_once and len(TERM_SIGNAL_RECEIVED) > 1:
+            return
+
         raise TermSignalException(signame, signum, frame)
 
 def have_received_term_signal():
@@ -39,10 +53,22 @@ def have_received_term_signal():
 
     return TERM_SIGNAL_RECEIVED
 
-def register_term_signal_handlers(raise_exc):
+def register_term_signal_handlers(raise_exc, raise_once=True):
     """
     Register term signal handlers.
+
+    Args:
+        raise_exc: If True,
+            then the registered term signal handlers
+            will raise TermSignalException
+            when a term signal is received.
+        raise_once: If raise_exc is True and raise_once is True,
+            then the registered term signal handlers
+            will raise TermSignalException
+            only the first time a term signal is received.
     """
 
     for signame in TERM_SIGNALS:
-        signal.signal(getattr(signal, signame), partial(handle_term_signal, signame, raise_exc))
+        signum = getattr(signal, signame)
+        handler = partial(_handle_term_signal, signame, raise_exc, raise_once)
+        signal.signal(signum, handler)
